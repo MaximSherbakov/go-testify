59d3f94ecf7f3f6883d617e379705a3e4094029d
Maxim Shcherbakov <maxim.1sherbakov1@gmail.com>
2024-10-04 01:16:15 +0300

    First commit

---
+++ precode_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
+	totalCount := 4
+	req := httptest.NewRequest("GET", "/cafe?city=moscow&count=10", nil)
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	// здесь нужно добавить необходимые проверки
+	require.Equal(t, http.StatusOK, responseRecorder.Code)
+	assert.Equal(t, strings.Join(cafeList["moscow"], ","), responseRecorder.Body.String())
+	responeBody := responseRecorder.Body.String()
+	responseCount := len(strings.Split(responeBody, ","))
+	assert.Equal(t, totalCount, responseCount)
+}
